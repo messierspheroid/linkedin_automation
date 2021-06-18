812fe22bc4c8de86cd262e48fb660c1fb8ecc54e
messierspheroid <chad.bjornberg@gmail.com>
2021-06-17 19:25:54 -0700

    test console diff

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+driver = webdriver.Chrome("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe")
+driver.get("https://linkedin.com")
